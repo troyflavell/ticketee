b71a70c7c4ef69f25231f0c557c0cef9a37a7c6d
troyflavell <troyflavel@gmail.com>
2017-03-25 00:28:58 +1300

    idk

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,26 @@
 class ProjectsController < ApplicationController
-	
+
 	def index
 		@projects = Project.all
 end
 
 def show
 @project = Project.find(params[:id])
+end
+
+def edit
+@project = Project.find(params[:id])
+end
+
+def update
+@project = Project.find(params[:id])
+if @project.update(project_params)
+flash[:notice] = "Project has been updated."
+redirect_to @project
+else
+flash.now[:alert] = "Project has not been updated."
+render "edit"
+end
 end
 
 
@@ -35,4 +50,3 @@ private
 def project_params
 params.require(:project).permit(:name, :description)
 end
-
